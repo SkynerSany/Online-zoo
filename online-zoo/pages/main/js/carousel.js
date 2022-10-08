32e5003fe4792e199354dd68b34bdf5fc857a68a
Alexandr Rylkov <skynerstudent@gmail.com>
2022-10-08 18:36:07 +0300

    feat: add scroll on sliders

--- online-zoo/pages/main/js/carousel.js
+++ online-zoo/pages/main/js/carousel.js
@@ -8,10 +8,9 @@ export default class Carousel {
     this.slider = document.querySelector(slider);
     this.sliderItem = sliderItem;
     this.createDomElement = new CreateDomElement(data, this.slider);
-    this.touchPosition = 0;
-    this.prevPosition = 0;
-    this.position = 0;
   }
+
+  position = 0;
 
   setDisabled(btn, timeOut) {
     btn.disabled = true;
@@ -19,53 +18,36 @@ export default class Carousel {
   }
 
   setNextItem() {
-    ++this.position;
+    this.position += this.sliderItem.offsetWidth + parseFloat(getComputedStyle(this.slider).columnGap);
+    console.log()
 
-    if (this.position === Math.ceil(this.slider.childNodes.length / 2)) {
-      this.position = -Math.floor(this.slider.childNodes.length / 2);
+    if (this.position >= this.slider.scrollWidth) {
+      this.position = 0;
     }
 
-    this.slider.style["transform"] = `translateX(${-this.position}00%)`;
+    this.slider.style["transform"] = `translateX(${-this.position}px)`;
 
     this.setDisabled(this.btnNext, 500);
   }
 
   setPrevItem() {
-    --this.position;
+    this.position -= this.sliderItem.offsetWidth + parseFloat(getComputedStyle(this.slider).columnGap);
 
-    if (this.position <= -Math.ceil(this.slider.childNodes.length / 2)) {
-      this.position = Math.floor(this.slider.childNodes.length / 2);
+    if (this.position < 0) {
+      this.position = this.slider.scrollWidth - this.sliderItem.offsetWidth;
     }
 
-    this.slider.style.transform = `translateX(${-this.position}00%)`;
+    this.slider.style.transform = `translateX(${-this.position}px)`;
     this.setDisabled(this.btnPrev, 500);
   }
+  
 
-  setCurrentItem(touchPosition) {
-    const itemHeight = this.sliderItem.offsetHeight + parseFloat(getComputedStyle(this.slider).rowGap);
+  setCurrentItem() {
     const itemWidth = this.sliderItem.offsetWidth + parseFloat(getComputedStyle(this.slider).columnGap);
     
     this.position = this.inputRange.value;
 
-    if (touchPosition) {
-      if (this.prevPosition + (this.touchPosition - touchPosition) < 0) {
-
-        this.slider.style.transform = `translateY(0px)`;
-        this.prevPosition = 0;
-
-      } else if (this.prevPosition + (this.touchPosition - touchPosition) > itemHeight * this.inputRange.max) {
-
-        this.slider.style.transform = `translateY(-${itemHeight * this.inputRange.max}px)`;
-        this.prevPosition = itemHeight * this.inputRange.max;
-
-      } else {
-
-        this.slider.style.transform = `translateY(-${this.prevPosition + (this.touchPosition - touchPosition)}px)`;
-
-      }
-    } else {
-      this.slider.style.transform = `translateX(${-itemWidth * this.position}px)`;
-    }
+    this.slider.style.transform = `translateX(${-itemWidth * this.position}px)`;
   }
 
   setEvents(itemsCount, itemsInItemsCount) {
@@ -79,11 +61,5 @@ export default class Carousel {
     this.btnNext?.addEventListener('click', () => this.setNextItem());
     this.btnPrev?.addEventListener('click', () => this.setPrevItem());
     this.inputRange?.addEventListener('input', () => this.setCurrentItem());
-    
-    if (this.inputRange && windowWidth <= 640) {
-      this.slider.addEventListener('touchstart', (e) => this.touchPosition = e.touches[0].pageY);
-      this.slider.addEventListener('touchmove', (e) => {e.preventDefault(); this.setCurrentItem(e.touches[0].pageY)});
-      this.slider.addEventListener('touchend', (e) => {this.prevPosition += (this.touchPosition - e.changedTouches[0].pageY);});
-    }
   }
 };
